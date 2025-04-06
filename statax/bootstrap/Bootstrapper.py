a08c4454b305da58cac3ae9fbe70f2ff7de8b34f
jack-norrie <jacknorrie6785@gmail.com>
2025-04-06 18:50:52 +0100

    Fix resamples not being set

--- statax/bootstrap/Bootstrapper.py
+++ statax/bootstrap/Bootstrapper.py
@@ -2,7 +2,10 @@ from abc import ABC, abstractmethod
 from typing import Callable
 from enum import StrEnum, auto
 
+import jax
+from jax import Array, random
 import jax.numpy as jnp
+from jaxlib.mlir.ir import Value
 
 
 class CITypes(StrEnum):
@@ -13,20 +16,41 @@ class CITypes(StrEnum):
 
 class Bootstrapper(ABC):
     def __init__(self, statistic: Callable):
-        self._statistic = statistic
-        self._bootstrap_replicates = None
+        self._statistic = jax.jit(statistic)
+        self._bootstrap_replicates: jax.Array | None = None
 
-    def resample(
-        self, data: jnp.ndarray, n_resamples: int = 2000, seed: int = 42
-    ) -> None:
-        pass
+    @property
+    def bootstrap_replicates(self) -> jax.Array:
+        bootstrap_replicates = self._bootstrap_replicates
+        if bootstrap_replicates is None:
+            raise ValueError("Bootstrap replicates have not been generated yet. You must call resample() first.")
+        return bootstrap_replicates
+
+    def _resample_data(self, data: Array, rng_key: jax.Array):
+        _, rng_subkey = random.split(rng_key)
+        resampled_idxs = random.choice(rng_subkey, jnp.arange(len(data)), shape=(len(data),), replace=True)
+        data_resampled = data.at[resampled_idxs].get()
+        return data_resampled
+
+    def resample(self, data: jax.Array, n_resamples: int = 2000, seed: int = 42) -> None:
+        rng_key = random.key(seed)
+
+        theta_hat = self._statistic(data)
+
+        self._bootstrap_replicates = jnp.empty((n_resamples, *theta_hat.shape))
+        for b in range(n_resamples):
+            rng_key, rng_subkey = random.split(rng_key)
+            data_resampled = self._resample_data(data, rng_subkey)
+
+            theta_boot = self._statistic(data_resampled)
+            self._bootstrap_replicates = self._bootstrap_replicates.at[b].set(theta_boot)
 
     def variance(self):
         pass
 
     @abstractmethod
-    def ci(self, size: float, alternative: CITypes):
-        pass
+    def ci(self, size: float, alternative: CITypes) -> tuple[float, float]:
+        raise NotImplementedError
 
     def plot_bootstrap_distribution(self) -> None:
         pass
---
+++ tests/unit/bootstrap/test_Bootstrapper.py
@@ -0,0 +1,55 @@
+import jax.numpy as jnp
+from jax import random
+import pytest
+
+from statax.bootstrap.Bootstrapper import Bootstrapper, CITypes
+
+
+class MockBootstrapper(Bootstrapper):
+    def ci(self, size: float, alternative: CITypes = CITypes.TWO_SIDED) -> tuple[float, float]:
+        return (0.0, 1.0)
+
+
+@pytest.fixture(scope="function")
+def median_bootstrapper():
+    return MockBootstrapper(jnp.median)
+
+
+class TestBootstrapper:
+
+    def test_data_resampling(self, median_bootstrapper):
+        rng_key = random.key(42)
+        rng_key, rng_subkey = random.split(rng_key)
+
+        data = jnp.arange(10)
+        data_resampled = median_bootstrapper._resample_data(data, rng_key)
+
+        assert data.shape == data_resampled.shape
+
+    def test_resampling(self, median_bootstrapper):
+        n_resamples = 5
+        data = jnp.arange(10)
+        median_bootstrapper.resample(data, n_resamples=n_resamples)
+
+        assert len(median_bootstrapper.bootstrap_replicates) == n_resamples
+
+    def test_resampling_variability(self, median_bootstrapper):
+        n_resamples = 100
+        data = jnp.arange(10)
+        print(data)
+        median_bootstrapper.resample(data, n_resamples=n_resamples)
+
+        all_equal = True
+        first_value = median_bootstrapper.bootstrap_replicates[0]
+        for b in range(1, n_resamples):
+            bootstrap_replicate = median_bootstrapper.bootstrap_replicates[b]
+            print(bootstrap_replicate)
+            if not jnp.allclose(first_value, bootstrap_replicate):
+                all_equal = False
+                break
+
+        assert not all_equal
+
+
+if __name__ == "__main__":
+    pytest.main()
