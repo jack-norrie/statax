7babe4099c87895fd8c38627569c092eb52c8376
jack-norrie <jacknorrie6785@gmail.com>
2025-04-06 21:41:10 +0100

    Add standard bootstrapper

---
+++ statax/bootstrap/StandardBootstrapper.py
@@ -0,0 +1,23 @@
+from statax.bootstrap.Bootstrapper import Bootstrapper
+from statax.bootstrap.types import CIType
+
+import jax.numpy as jnp
+from jax.scipy.stats import norm
+
+
+class TBootstrapper(Bootstrapper):
+    def ci(self, confidence_level: float, alternative: CIType) -> tuple[float, float]:
+        alpha = 1 - confidence_level
+        if alternative == CIType.TWO_SIDED:
+            low = self.theta_hat + norm.ppf(alpha / 2) * self.variance()
+            high = self.theta_hat + norm.pdf(1 - alpha / 2) * self.variance()
+        elif alternative == CIType.LESS:
+            low = -jnp.inf
+            high = self.theta_hat + norm.pdf(1 - alpha) * self.variance()
+        elif alternative == CIType.GREATER:
+            low = self.theta_hat + norm.ppf(alpha) * self.variance()
+            high = jnp.inf
+        else:
+            raise ValueError(f"Invalid alternaive passed, must be of type: {CIType}")
+
+        return (float(low), float(high))
