e44d20fbb515ad19269478e0defe23edaca48381
jack-norrie <jacknorrie6785@gmail.com>
2025-04-07 21:05:09 +0100

    Add bootstrap API

---
+++ statax/bootstrap/__init__.py
@@ -0,0 +1,15 @@
+from .BasicBootstrap import BasicBootstrapper
+from .TBootstrapper import TBootstrapper
+from .StandardBootstrapper import StandardBootstrapper
+from .PercentileBootstrapper import PercentileBootstrapper
+from .BCBootstrapper import BCBootstrapper
+from .BCaBootstrapper import BCaBootstrapper
+
+__all__ = [
+    "BasicBootstrapper",
+    "TBootstrapper",
+    "StandardBootstrapper",
+    "PercentileBootstrapper",
+    "BCBootstrapper",
+    "BCaBootstrapper",
+]
